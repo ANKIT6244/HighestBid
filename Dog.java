7934ea44487c95ae38ad9414c63f1251865842bc
ANKIT6244 <ankitkumarshah5050@gmail.com>
2024-01-09 16:39:37 +0530

    getter & setter

---
+++ Dog.java
@@ -0,0 +1,30 @@
+public class Dog {
+
+    private String breed;
+    private int Size;
+    private String Colour;
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBread(String breed) {
+        this.breed = breed;
+    }
+
+    public int getsize() {
+        return Size;
+    }
+
+    public void setSize(int Size) {
+        this.Size = Size;
+    }
+
+    public String getColour() {
+        return Colour;
+    }
+
+    public void setColour(String Colour) {
+        this.Colour = Colour;
+    }
+}
---
+++ mainDog.java
@@ -0,0 +1,14 @@
+public class mainDog {
+    public static void main(String[] args) {
+
+        Dog Do1 = new Dog();
+
+        Do1.setBread("\n German sheferd");
+        Do1.setSize(56);
+        Do1.setColour("BrowinshWhite");
+
+        System.out.println(Do1.getBreed());
+        System.out.println(Do1.getsize());
+        System.out.println(Do1.getColour());
+    }
+}
